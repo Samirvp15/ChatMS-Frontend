4c3cd40a65fa16366a777521c91f0eb2fb0c9c87
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2025-01-31 16:32:06 -0500

    add reducers and dispatchUser

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,46 +1,56 @@
 
 import axios from 'axios';
 import { useEffect } from 'react';
-import { Outlet } from 'react-router';
-import { useAppSelector } from '../hooks/reduxHook';
+import { Outlet, useNavigate } from 'react-router';
+import { useAppDispatch, useAppSelector } from '../hooks/reduxHook';
+import { logout, setUser } from '../redux/userSlice';
 
 
 export default function Home() {
 
   const user = useAppSelector(state => state.user)
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
 
   console.log(user)
 
-  const fetchUserDetails = async()=>{
+  const fetchUserDetails = async () => {
 
     try {
       const url = `${import.meta.env.VITE_BACKEND_URL}/api/user-details`
 
-      await axios({
+      const response = await axios({
         url: url,
         withCredentials: true
       })
- 
+
+      dispatch(setUser(response.data.data))
+
+      if (response.data.logout) {
+        dispatch(logout())
+        navigate('/email')
+      }
+
     } catch (error) {
       if (axios.isAxiosError(error)) {
-       console.log(error)
+        console.log(error)
       }
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchUserDetails()
-  },[])
+  }, [])
 
 
   return (
     <>
-    <div>
+      <div>
         Home
-      <section>
-        <Outlet />
-      </section>
-    </div>
+        <section>
+          <Outlet />
+        </section>
+      </div>
     </>
   )
 }
